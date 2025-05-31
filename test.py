f60b5c168927e5a1653536a8d297cdabd94e2d88
Florian <florian@zager-online.de>
2025-05-31 22:18:58 +0200

    added logs

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 
 import models.ghostnetv3 as ghostnetv3
+import models.ghostnetv3_small as ghostnetv3_small
 from models.resnet import resnet50, resnet34, resnet18
 from models.densenet import densenet121, densenet161, densenet169
 from models.inception import inception_v3
@@ -25,7 +26,7 @@ def main():
     #model = resnet18(pretrained=True, device=device).to(device)
     model = timm.create_model('ghostnetv3_small', width=2.8, num_classes=10)
     checkpoint = torch.load("default_ghostnetv3_small_cifar10.pth", map_location=device)
-    model.load_state_dict(checkpoint['model_state_dict'])
+    model.load_state_dict(checkpoint)
     model.eval()
 
     print(sum(p.numel() for p in model.parameters()))
